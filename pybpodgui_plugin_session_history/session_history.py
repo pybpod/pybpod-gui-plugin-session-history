c8d2a7ad1e434d0a6b89f3eb2d38115f5fd84563
Ricardo Ribeiro <ricardojvr@gmail.com>
2018-04-05 13:02:58 +0100

    Fix some bugs

--- pybpodgui_plugin_session_history/session_history.py
+++ pybpodgui_plugin_session_history/session_history.py
@@ -115,50 +115,38 @@ class PandasModel(QAbstractTableModel):
         return Qt.ItemIsEnabled
 
 
+
+
+
 class SessionHistory(BaseWidget):
     """ Plugin main window """
 
     def __init__(self, session):
         BaseWidget.__init__(self, session.name)
-        
         self.set_margin(5)
 
-        self._progress    = ControlProgress('Progress', visible=False)
-        self._reload      = ControlButton('Reload everything', default=self.__reload_evt)
-        self._autoscroll  = ControlCheckBox('Auto-scroll', default=True,  changed_event=self.__auto_scroll_evt)
+        self._autoscroll  = ControlCheckBox('Auto-scroll', default=True)
         self._log         = ControlTableView(select_entire_row=True)
 
         self._formset = [
-            ('_autoscroll',' ',' ','_reload',),
-            '_log',
-            '_progress'
+            ('_autoscroll',' ',' ',' ',),
+            '_log'
         ]
 
-        self.session = session
-        self._colors = {}
-
-        self._progress.hide()
-
-        self._timer = QTimer()
-
+        self.session    = session
         self._log.value = self.model = PandasModel(session.data)
+        
+        self._timer = QTimer()
         self._timer.timeout.connect(self.__update_table_view)
 
     def __update_table_view(self):
+        # check if the session is running. If not stops the timer
+        if not self.session.is_running: self._timer.stop()
+
         self._log.value = None
         self._log.value = self.model
-        if self._autoscroll.value:
-            self._log.scrollToBottom()
+        if self._autoscroll.value: self._log.scrollToBottom()
 
-    def __reload_evt(self):
-        self._history_index = 0
-        #self._log.clear()
-        self._stop = False # flag used to close the gui in the middle of a loading
-        self.read_message_queue(True)
-
-    def __auto_scroll_evt(self):
-        #self._log.autoscroll = self._autoscroll.value
-        pass
     
     def show(self, detached=False):
         if self.session.is_running and self.session.setup.detached:
@@ -175,16 +163,8 @@ class SessionHistory(BaseWidget):
         else:
             BaseWidget.show(self)
 
-        #in case the session is still running load the latest 200 messages
-        if self.session.is_running:
-            self._history_index = len(self.session.messages_history)-200 if len(self.session.messages_history)>200 else 0
-            self._reload.enabled = False
-        else:
-            self._history_index  = 0
-            self._reload.enabled = True
-
+        
         self._stop = False # flag used to close the gui in the middle of a loading
-        self.read_message_queue(True)
         if not self._stop and self.session.is_running:
             self._timer.start(conf.SESSIONLOG_PLUGIN_REFRESH_RATE)
 
@@ -198,124 +178,10 @@ class SessionHistory(BaseWidget):
         self.session.sessionhistory_action.setEnabled(True)
         self.session.sessionhistory_detached_action.setEnabled(True)
 
-    def read_message_queue(self, update_gui=False):
-        """ Update board queue and retrieve most recent messages """
-
-        # check if the session is running. If not stops the timer
-        if not self.session.is_running: self._timer.stop()
-
-        messages_history = self.session.messages_history
-        recent_history   = messages_history[self._history_index:]
-
-        if update_gui:
-            self._progress.show()
-            self._progress.value = 0
-            self._progress.max   = len(recent_history)
-        try:
-
-            self._log.form.setUpdatesEnabled(False)
-
-            for msg in recent_history:
-                if self._stop: return
-                row = None
-
-                if issubclass( type(msg), StderrMessage):
-                    row = [
-                        self._history_index, 
-                        msg.MESSAGE_TYPE_ALIAS, 
-                        str(msg.content) + ' | ' + str(msg.traceback),
-                        '-',
-                        str(msg.host_timestamp) if msg.host_timestamp else '-',
-                        '-', 
-                        str(msg.pc_timestamp)
-                    ]
-
-                elif issubclass(type(msg), StateOccurrence):
-                    row = [
-                        self._history_index, 
-                        msg.MESSAGE_TYPE_ALIAS, 
-                        msg.content,
-                        '-',
-                        str(msg.start_timestamp),
-                        str(msg.end_timestamp), 
-                        str(msg.pc_timestamp)
-                    ]
-
-                elif issubclass(type(msg), SessionInfo):
-                    row = [
-                        self._history_index, 
-                        msg.MESSAGE_TYPE_ALIAS, 
-                        "{0}={1}".format(msg.infoname,msg.infovalue) if msg.infovalue else msg.infoname,
-                        '-',
-                        '-',
-                        '-', 
-                        str(msg.pc_timestamp)
-                    ]
-                elif issubclass(type(msg), (EventOccurrence, EventResume)):
-                    row = [
-                        self._history_index, 
-                        msg.MESSAGE_TYPE_ALIAS, 
-                        msg.event_name,
-                        msg.event_id,
-                        str(msg.host_timestamp) if msg.host_timestamp is not None else '-',
-                        '-', 
-                        str(msg.pc_timestamp)
-                    ]                   
-                else:
-                    row = [
-                        self._history_index, 
-                        msg.MESSAGE_TYPE_ALIAS, 
-                        str(msg.content),
-                        '-',
-                        str(msg.host_timestamp) if msg.host_timestamp else '-',
-                        '-', 
-                        str(msg.pc_timestamp)
-                    ]
-
-
-                if row:
-                    self._log += row
-
-                    if msg.MESSAGE_COLOR:
-                        if msg.MESSAGE_COLOR not in self._colors:
-                            color = self._colors[msg.MESSAGE_COLOR] = QColor(*msg.MESSAGE_COLOR)
-                        else:
-                            color = self._colors[msg.MESSAGE_COLOR]
-
-                        for i in range(len(row)):
-                            self._log.get_cell(i, len(self._log)-1 ).setForeground(color)
-                    
-                    if update_gui:  self._progress += 1
-
-
-        
-                QApplication.processEvents()
-                self._history_index += 1
-
-            self._log.form.setUpdatesEnabled(True)
-
-        
-            
-        except Exception as err:
-            if hasattr(self, '_timer'):
-                self._timer.stop()
-            logger.error(str(err), exc_info=True)
-            self.alert("Unexpected error while loading session history. Pleas see log for more details.", "Error")
-
-        if update_gui:
-            self._progress.hide()
-
-        if not self.session.is_running:
-            self._reload.enabled = True
+    @property
+    def mainwindow(self): return self.session.mainwindow
 
     @property
-    def mainwindow(self):
-        return self.session.mainwindow
-
-    @property
-    def title(self):
-        return BaseWidget.title.fget(self)
-
+    def title(self): return BaseWidget.title.fget(self)
     @title.setter
-    def title(self, value):
-        BaseWidget.title.fset(self, 'Session History: {0}'.format(value))
+    def title(self, value):  BaseWidget.title.fset(self, 'Session History: {0}'.format(value))
