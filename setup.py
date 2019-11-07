23a2005832602ad94034ee9c24a15d2a8c8fc247
Luís Teixeira <micboucinha@gmail.com>
2019-11-07 18:20:57 +0000

    Update license and maintainers on setup.py

--- setup.py
+++ setup.py
@@ -6,26 +6,26 @@ import re
 
 version = ''
 with open('pybpodgui_plugin_session_history/__init__.py', 'r') as fd:
-	version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-	                    fd.read(), re.MULTILINE).group(1)
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
 if not version:
-	raise RuntimeError('Cannot find version information')
+    raise RuntimeError('Cannot find version information')
 
 setup(
-	name='pybpod-gui-plugin-session-history',
-	version=version,
-	description="""PyBpod GUI session log plugin""",
-	author='Carlos Mão de Ferro, Ricardo Jorge Vieira Ribeiro',
-	author_email='cajomferro@gmail.com, ricardojvr@gmail.com',
-	license='Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>',
-	url='https://bitbucket.org/fchampalimaud/pybpod-gui-plugin-session-history',
+    name='pybpod-gui-plugin-session-history',
+    version=version,
+    description="""PyBpod GUI session log plugin""",
+    author='Carlos Mão de Ferro, Ricardo Jorge Vieira Ribeiro, Luís Teixeira',
+    author_email='cajomferro@gmail.com, ricardojvr@gmail.com, micboucinha@gmail.com',
+    license='MIT',
+    url='https://github.com/pybpod/pybpod-gui-plugin-session-history',
 
-	include_package_data=True,
-	packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples', 'deploy', 'reports']),
+    include_package_data=True,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples', 'deploy', 'reports']),
 
-	package_data={'pybpodgui_plugin_session_history': [
-		'resources/*.*',
-	]
-	},
+    package_data={'pybpodgui_plugin_session_history': [
+        'resources/*.*',
+    ]
+    },
 )
